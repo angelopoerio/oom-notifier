09e5d342e30c637f979cdf46ecd407048eba1300
Angelo Poerio <angelopoerio@localhost.localdomain>
2021-06-27 21:02:24 +0200

    first commit

--- oom_trigger.c
+++ oom_trigger.c
@@ -3,15 +3,16 @@
 #include <stdlib.h>
 
 int main (void) {
-				int n = 0;
-				char *p;
+	int n = 0;
+	char *p;
 
-				while (1) {
-								if ((p = malloc(1<<20)) == NULL) {
-												printf("malloc failure after %d MiB\n", n);
-												return 0;
-								}
-								memset (p, 0, (1<<20));
-								printf ("got %d MiB\n", ++n);
-				}
+	while (1) {
+		if ((p = malloc(1<<20)) == NULL) {
+			printf("malloc failure after %d MiB\n", n);
+			return 0;
+		}
+		
+		memset (p, 0, (1<<20));
+		printf ("got %d MiB\n", ++n);
+	}
 }
