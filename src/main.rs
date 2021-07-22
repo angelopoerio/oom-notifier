877b7ce1fa3bb49d1354301625b47722a32b0949
Angelo Poerio <angelopoerio@localhost.localdomain>
2021-07-22 19:02:12 +0200

    better logic to determine hostname

--- src/main.rs
+++ src/main.rs
@@ -30,16 +30,18 @@ fn get_pid_max() -> usize {
     return content.trim().parse::<usize>().unwrap();
 }
 
-fn build_oom_event(pid: i32, cmdline: String) -> serde_json::Value {
-    let mut machine_hostname = "";
+fn get_hostname() -> String {
     match env::var("HOSTNAME") {
-        Ok(val) => machine_hostname = &val,
+        Ok(val) => return val,
         Err(_) => match fs::read_to_string("/proc/sys/kernel/hostname") {
-            Ok(host_name) => machine_hostname = &host_name.trim().to_string(),
-            Err(e) => machine_hostname = &e.to_string(),
+            Ok(host_name) => return host_name.trim().to_string(),
+            Err(e) => return e.to_string(),
         },
     }
-    let message = json!({ "cmdline": cmdline, "pid": pid.to_string(), "hostname":machine_hostname,
+}
+
+fn build_oom_event(pid: i32, cmdline: String) -> serde_json::Value {
+    let message = json!({ "cmdline": cmdline, "pid": pid.to_string(), "hostname": get_hostname(),
                 "time": std::time::SystemTime::now()
                 .duration_since(std::time::UNIX_EPOCH)
                 .unwrap()
